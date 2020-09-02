bd7e6568a11d204d7c371c325ab1a93df238f55d
Haris <hhaskic1@etf.unsa.ba>
2020-09-02 16:55:40 +0200

    Added some class

---
+++ src/ba/unsa/etf/rpr/projekat/RegisterController.java
@@ -0,0 +1,17 @@
+package ba.unsa.etf.rpr.projekat;
+
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class RegisterController {
+
+    public TextField fieldUsername;
+    public TextField fieldPassword;
+    public Button buttonLogIn;
+    public Button buttonExit;
+
+
+
+
+}
